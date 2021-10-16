8c1e440f7c79dc910aa2fac0051a6326e9cc6cd2
KillWolfVlad <KillWolfVlad@users.noreply.github.com>
2021-10-16 11:05:56 +0500

    feat: add service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { ProductDto } from '@enterprise-scope/some-dtos';
+import { Controller, Post } from '@nestjs/common';
 
-@Controller()
+@Controller({
+  path: '/products',
+  version: '1',
+})
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('/')
+  public createProduct(): Promise<ProductDto> {
+    throw new Error('NotImplementedException');
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
+
+function setupSwagger(app: INestApplication): void {
+  const options = new DocumentBuilder().build();
+
+  SwaggerModule.setup('api', app, SwaggerModule.createDocument(app, options));
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  setupSwagger(app);
+
   await app.listen(3000);
 }
+
 bootstrap();
